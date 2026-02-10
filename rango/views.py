475fe9f7af259e5f39dc9c8d13f8d4b3b934ce78
yuhang-feng404 <160128050+yuhang-feng404@users.noreply.github.com>
2026-02-10 20:45:56 +0000

    Fix static files and add Rango image

---
+++ rango/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def index(request):
+    context_dict = {
+        'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'
+    }
+    return render(request, 'rango/index.html', context=context_dict)
